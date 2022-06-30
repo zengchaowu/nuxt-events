9fb6d8b0a2e5b16d37653ca0ce8ade4a4cd5836e
zengchaowu <root@zengchaowu.com>
2022-06-30 18:13:59 +0800

    chore: 将读写nuxt.config.js的操作放入到单独的bin下面

---
+++ bin/setup.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import write from "@doraemon-module/nuxt-functions/lib/write";
+
+write();
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import fs from "fs";
 import path from "path";
 import copy from "@doraemon-module/nuxt-functions/lib/copy";
 import mkdirp from "@doraemon-module/nuxt-functions/lib/mkdirp";
-import write from "@doraemon-module/nuxt-functions/lib/write";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import camelcase from "camelcase";
@@ -46,6 +45,4 @@ export default async () => {
     'import EventEmitter from "events"; export const emitter = new EventEmitter(); export default ' +
       json
   );
-
-  write();
 };
