3cd19ac5722b84c5565ccd851214737dd020bc5f
zengchaowu <root@zengchaowu.com>
2022-06-30 20:45:05 +0800

    chore: setup增加注入数组的能力

--- bin/setup.js
+++ bin/setup.js
@@ -1,4 +1,17 @@
 #!/usr/bin/env node
-import write from "@doraemon-module/nuxt-functions/lib/write";
+import ts from "typescript";
+import write from "@doraemon-module/nuxt-functions/lib/write.js";
 
-write();
+write(undefined, undefined, "buildModules", (node) => {
+  const name = "@doraemon-module/nuxt-events";
+  let isExist = false;
+  for (let element of node.elements) {
+    if (element.text === name) {
+      isExist = true;
+      break;
+    }
+  }
+  if (!isExist) {
+    node.elements.push(ts.createStringLiteral(name));
+  }
+});
