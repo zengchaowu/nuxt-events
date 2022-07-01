21ca64a5fce94250339869661a07ecaf8d51f175
zengchaowu <root@zengchaowu.com>
2022-07-01 11:29:22 +0800

    chore: remove setup

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 import fs from "fs";
 import path from "path";
-import copy from "@doraemon-module/nuxt-functions/lib/copy";
 import mkdirp from "@doraemon-module/nuxt-functions/lib/mkdirp";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import camelcase from "camelcase";
 
 export default async () => {
-  const __dirname = dirname(fileURLToPath(import.meta.url));
-
   const events = mkdirp(path.join(process.cwd(), "events"));
-  copy(
-    path.join(__dirname, "lib", "_gitignore"),
-    path.join(events, ".gitignore")
-  );
-
   const lib = mkdirp(path.join(events, "lib"));
-  copy(
-    path.join(__dirname, "lib", "application.js"),
-    path.join(lib, "application.js")
-  );
 
   const files = fs.readdirSync(lib);
   const out = {};
